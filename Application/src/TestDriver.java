3c74f87c675e498fc37e5186503d9aaaf839f11c
trons <kakotamix@hotmail.com>
2013-11-30 17:09:25 +0000

    Triple Class: Data structure to store the data (see javadoc comments for more ore details).
    Model Class: first stub of the model. At the moment it loads data from
    disk and stores the data into a Triple structure.
    
    TestDriver: test driver program to check how the thing works.

---
+++ Application/src/TestDriver.java
@@ -0,0 +1,22 @@
+
+public class TestDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		long t1 = System.currentTimeMillis();
+		Model test = new Model(args[0]);
+		System.out.println(System.currentTimeMillis() - t1 + " milliseconds");
+		
+		/*
+		 * The following two lines show how to access the data:
+		 * 		1.- how to get all the tuples <country, value> for a certain variable.
+		 * 		2.- how to get a certain value for a certain country in a certain variable.
+		 */
+		 
+		System.out.println(test.getTriple().getVariable("isoCode"));
+		System.out.println(test.getTriple().getValue("isoCode", "Nicaragua"));
+	}
+
+}
