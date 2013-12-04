a36f27d424475c142df2cb566081b21995e038c6
MLBown <Matthew@Matthew-PC>
2013-12-04 22:54:16 +0000

    Removed test code

--- Application/src/BarchartCluster.java
+++ Application/src/BarchartCluster.java
@@ -172,70 +172,7 @@ public class BarchartCluster extends JPanel{
 		datasets[index].removeColumn(country.substring(0,4));
 	}
 	
-	/*
-	 * Method to test barchartCluster
-	 */
-	public void test(){
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		dataset.setValue(6, "Profit", "Jane");
-		dataset.setValue(7, "Profit", "Tom");
-		dataset.setValue(8, "Profit", "Jill");
-		dataset.setValue(5, "Profit", "John");
-		dataset.setValue(12, "Profit", "Fred");
-		
-		DefaultCategoryDataset dataset2 = new DefaultCategoryDataset();
-		dataset2.setValue(6, "Sex appeal", "Jane");
-		dataset2.setValue(56, "Sex appeal", "Tom");
-		dataset2.setValue(8, "Sex appeal", "Jill");
-		dataset2.setValue(2, "Sex appeal", "John");
-		dataset2.setValue(16, "Sex appeal", "Fred");
-		
-		DefaultCategoryDataset dataset3 = new DefaultCategoryDataset();
-		dataset3.setValue(90, "Height", "Jane");
-		dataset3.setValue(11, "Height", "Tom");
-		dataset3.setValue(100, "Height", "Jill");
-		dataset3.setValue(150, "Height", "John");
-		dataset3.setValue(19, "Height", "Fred");
-		
-		DefaultCategoryDataset dataset4 = new DefaultCategoryDataset();
-		dataset4.setValue(6, "Hand Span", "Jane");
-		dataset4.setValue(56, "Hand Span", "Tom");
-		dataset4.setValue(23, "Hand Span", "Jill");
-		dataset4.setValue(53, "Hand Span", "John");
-		dataset4.setValue(12, "Hand Span", "Fred");
-		
-		this.addChart(0, "Profit", dataset);
-		this.addChart(1, "Sex Appeal", dataset2);
-		this.addChart(2, "Height", dataset3);
-		this.addChart(3, "Hand Span", dataset4);
-		
-	}
-	
-	/*
-	 * Test method to initialise the graphs so they can accept countries being added.
-	 */
-	public void countryAddTest(){
-		DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
-		DefaultCategoryDataset dataset2 = new DefaultCategoryDataset();
-		DefaultCategoryDataset dataset3 = new DefaultCategoryDataset();
-		DefaultCategoryDataset dataset4 = new DefaultCategoryDataset();
-		datasets[0] = dataset1;
-		datasets[1] = dataset2;
-		datasets[2] = dataset3;
-		datasets[3] = dataset4;
-		labels[0] = "One";
-		labels[1] = "Two";
-		labels[2] = "Three";
-		labels[3] = "Four";
-		
-		addChart(0, labels[0], dataset1);
-		addChart(1, labels[1], dataset2);
-		addChart(2, labels[2], dataset3);
-		addChart(3, labels[3], dataset4);
-	}
-	
-	public void getChart(int index){
-		
-	}
+
+
 	
 }
