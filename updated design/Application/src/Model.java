ec7ce0aa8947da2684b573b0f08eec17f68952b4
IoannisCleary <nicolemaniac@gmail.com>
2013-12-04 23:33:21 +0000

    updaed design - separate version

---
+++ updated design/Application/src/Model.java
@@ -0,0 +1,165 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
+/**
+ * Stub class for the model. At the moment it opens a .csv file and stores its content in memory.
+ * It also has a couple of methods to access the data from those classes that instantiate a Model
+ * object.
+ * 
+ * @author Georgios Moleski and Mario Moro Hernández
+ *
+ */
+public class Model {
+	
+	private Triple data;
+	private String[] variables;
+        private String[] labels;
+	private static ArrayList<String>  countries;
+	private HashMap<String,String> varlb = new HashMap<String,String>();
+	
+	/**
+	 * Parameterised constructor. Given a file name, it creates a Triple type data structure  
+	 * @param fileName
+	 */
+	public Model(String fileName){
+		data = new Triple();
+		countries = new ArrayList<String>();
+		process(fileName);
+                for (int i = 0; i<getVariables().length; i++)
+                    varlb.put(getLabels()[i],getVariables()[i]);
+	}
+	
+
+	
+	/**
+	 * Auxiliary method to open the file specified as a parameter in the process method.
+	 * @param path string representing the name of the .csv file containing the data
+	 * @return a scanner for the content of the file; null if the file is not found.
+	 */
+	private static Scanner openFile(String path){
+		File fd = new File(path);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fd));
+			return new Scanner(br);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	/**
+	 * Auxiliary method to insert the data in the .csv file into the Triple data structure.
+	 *  
+	 * @param line
+	 * @param triple
+	 * @param variables
+	 */
+	private static void parseLine(String line, Triple triple, String[] variables){
+		String[] tokens = line.split(",");
+		for (int i = 0; i < variables.length; i++){
+			String country = tokens[1];
+			if (!countries.contains(country))
+				countries.add(country);
+			for (int j = 0; j < tokens.length; j++){
+				if (triple == null){
+					triple = new Triple(variables[j],country,tokens[i]);
+				} else{
+					triple.put(variables[j], country, tokens[j]);
+				}
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Process the data contained in the .csv file passed as a parameter. This method
+	 * is to be called from the File->Open dialog box in the GUI.
+	 * @param file .csv file containing the dataset.
+	 */
+	public void process (String file) {
+		Scanner scn = openFile(file);
+		
+		boolean firstLine = false;
+                boolean secondLine = false;   
+		while (scn != null && scn.hasNextLine()){
+			String line = scn.nextLine();
+			if (!firstLine){
+				variables = line.split(",");				
+				firstLine = true;
+				continue;
+			}
+                        if (!secondLine){
+				labels = line.split(",");				
+				secondLine = true;
+				continue;
+			}
+			
+			parseLine(line, data, variables);
+			
+		}		
+	}
+	
+	
+	
+	/**
+	 * Accessor method to the Triple data structure containing the dataset. 
+	 * @return the Triple structure containing the dataset
+	 */
+	public Triple getTriple(){
+		return data;
+	}
+	
+	public ArrayList<String>getCountries(){       
+            return countries;
+            
+        }
+        
+        public HashMap<String,String> getVarLab(){
+            return varlb;
+        }
+	
+	public String[] getVariables(){
+            return variables;
+        }
+	public String[] getLabels(){
+            return labels;
+        }
+	/**
+	 * It returns an array list of arrays of the form [country, xValue, yValue, xLabel, yLabel]
+	 * to be used in the charts generation.
+	 * 
+	 * @param xVar variable on x-axis
+	 * @param yVar variable on y-axis
+	 * @param triple data structure containing the dataset
+	 * @return an array list of arrays [country, xValue, yValue, xLabel, yLabel]
+	 */
+	public ArrayList<String[]> get2VarDataArray(String xVar, String yVar, Triple triple){
+		
+		ArrayList<String[]> toReturn = new ArrayList<String[]>(triple.size());
+		
+		Iterator<String> it = triple.getVariable(xVar).keySet().iterator();
+				
+		while (it.hasNext()){	
+			String[] datum = new String[5];
+			String country = it.next(); 
+			datum[0] = country;//country
+			datum[1] = triple.getValue(xVar, country);//xval
+			datum[2] = triple.getValue(yVar, country);//yval
+			datum[3] = xVar;//xlabel
+			datum[4] = yVar;//ylabel
+			toReturn.add(datum);
+		}
+		
+		return toReturn;
+	}
+}
---
+++ updated design/Application/src/ScatterPlot.java
@@ -0,0 +1,125 @@
+import java.awt.Shape;
+import java.util.ArrayList;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.XYToolTipGenerator;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.util.ShapeUtilities;
+import org.jfree.chart.JFreeChart;
+
+/**
+ * Class for making scatter plots.
+ * @author Matthew Bown
+ *
+ */
+public class ScatterPlot extends ChartPanel {
+
+
+
+	private static final long serialVersionUID = 1L;
+
+
+	public ScatterPlot(String xVariable, String yVariable, String chartTitle, ArrayList<Double> xVal, ArrayList<Double> yVal, ArrayList<String> countries, int xSize, int ySize) {
+		super(null);
+		
+		ArrayList<XYDataset> dataset = new ArrayList<XYDataset>();
+		// This will create the dataset 
+		for(int i = 0; i < countries.size(); i++){
+			String country = countries.get(i);
+			double x =  xVal.get(i);
+			double y = yVal.get(i);
+
+			dataset.add(createDataset(country, x, y ));
+		}
+	      // based on the dataset we create the chart
+        JFreeChart chart = createChart(dataset,chartTitle, xVariable, yVariable);
+        // we put the chart into a panel
+        this.setChart(chart);
+        // default size
+        this.setPreferredSize(new java.awt.Dimension(xSize, ySize));
+    }
+
+    private  XYDataset createDataset(String country, double xVar, double yVar) {
+        XYSeriesCollection result = new XYSeriesCollection();
+        XYSeries series = new XYSeries(country);
+        series.setDescription(country);
+        series.add(xVar,yVar);
+        result.addSeries(series);
+        return result;
+        
+    }
+   
+
+ private JFreeChart createChart(ArrayList<XYDataset> dataset, String title, final String xVariable, final String yVariable) {
+	 XYPlot plot = new XYPlot();
+	 ValueAxis domain = new NumberAxis(xVariable);
+	 ValueAxis range = new NumberAxis(yVariable);
+
+
+	 plot.setDomainAxis(0,domain);
+	 plot.setRangeAxis(0,range);
+	 
+	 plot.mapDatasetToDomainAxis(0, 0);
+	 plot.mapDatasetToRangeAxis(0, 0);
+	 
+	 int i;
+	 for(i = 0; i < dataset.size(); i++){
+		 XYItemRenderer renderer = new XYLineAndShapeRenderer(false, true);
+		 Shape circle = ShapeUtilities.createDiamond((float) 5.5);
+
+		 XYToolTipGenerator tt1 = new XYToolTipGenerator() {
+	         public String generateToolTip(XYDataset dataset, int series, int item) {
+	            StringBuffer sb = new StringBuffer();
+	            Number x = dataset.getX(0, 0);
+	            Number y = dataset.getY(0, 0);
+	            XYSeriesCollection temp = (XYSeriesCollection) dataset;
+	            String s = temp.getSeries(0).getDescription();
+	            
+	            sb.append("<html><p style='color:blue;'>"+s+"</p><br />");
+	            if (xVariable != "Countries")
+	            sb.append(String.format("%s: %,.2f <br />",xVariable, x.doubleValue()));
+	            sb.append(String.format("%s: %,.2f</html>",yVariable, y.doubleValue()));
+	            return sb.toString();
+	         }
+	      };
+		 
+		 
+		 
+		 renderer.setBaseToolTipGenerator(tt1);;
+		 renderer.setSeriesShape(0,circle);
+		 plot.setDataset(i, dataset.get(i));
+		 plot.setRenderer(i, renderer);
+		 
+		 plot.mapDatasetToDomainAxis(0, 0);
+		 plot.mapDatasetToRangeAxis(0, 0);
+
+		
+	 }
+	 
+	 
+
+	 // Create the scatter data, renderer, and axis
+	
+	 // Create the chart with the plot and a legend
+	 JFreeChart chart;
+	 if(i <= 30){
+		 chart = new JFreeChart("", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+	 }
+	 else{
+		 chart = new JFreeChart("", JFreeChart.DEFAULT_TITLE_FONT, plot, false);
+	 }
+	 	chart.setTextAntiAlias(true);
+        return chart;
+        
+    }
+
+	
+	
+}
